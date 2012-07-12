54681485ced03a0c448820f41b2c887c1cf2b1c0
vivekshekar <vivek@sqlfusion.com>
2012-07-12 18:17:09 +0530

    Initial commin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '6104eb2e800ef6e7e78d7967f2e02ae3332855b73d8fd07306fd73bd811d9a57a192c87aab5d1a06bd1e89b36016c10625cd6df455b1943df794bf0fa5285a4b'
